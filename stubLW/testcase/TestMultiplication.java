0b77881f151d772497b8e97a7711bc07094a070c
Johnson Chao <chao.hs1031@gmail.com>
2019-05-16 15:03:30 +0800

    Update TestMultiplication.java

--- stubLW/testcase/TestMultiplication.java
+++ stubLW/testcase/TestMultiplication.java
@@ -73,29 +73,38 @@ public class TestMultiplication {
 
 		assertEquals(expectedANS, actualANS);
 	}
-/*
-	//@Test
+
+	@Test
 	public void testMultiplicationThreeNumberArrayList() {
 		// Test three number multiplications
 		// Here should init prepared answer
-
-		int expectedANS = 0;
+		int a = 2, b = 7, c = 11;
+		ArrayList<Integer> array = new ArrayList<Integer>(Arrays.asList(a, b, c));
+		
+		
+		int expectedANS = 154;
 
 		// Here should new Factorization object with Mockito stub
 		// Here should ask stub object to act whatever we want
 
+		Factorization factorizationStub = mock(Factorization.class);
+		when(factorizationStub.factorization(expectedANS)).thenReturn(array);
+		when(factorizationStub.factorizationRoot(anyInt(), anyBoolean())).thenReturn(-1);
+		
 		Multiplication multiplication = new Multiplication();
 		int actualANS = multiplication.multiplicationWithFactorizationAbsolute(factorizationStub, expectedANS);
 
 		assertEquals(expectedANS, actualANS);
 	}
 
-	//@Test
+	@Test
 	public void testMultiplicationOneNumberArrayList() {
 		// Test one number multiplications, complete test coverage to 100%
 		// Here should init prepared answer
+		int a = 10;
+		ArrayList<Integer> array = new ArrayList<Integer>(Arrays.asList(a));
 
-		int expectedANS = 0;
+		int expectedANS = -1;
 
 		// Here should new Factorization object with Mockito stub
 		// Here should ask stub object to act whatever we want
@@ -105,5 +114,5 @@ public class TestMultiplication {
 
 		assertEquals(expectedANS, actualANS);
 	}
-*/
+
 }
